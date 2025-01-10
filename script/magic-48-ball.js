8e63369a244399088ec93fe84ac6110d37fe9d51
TakeV <takev@disroot.org>
2025-01-10 02:17:02 -0500

    Deploy changes

--- script/magic-48-ball.js
+++ script/magic-48-ball.js
@@ -5,12 +5,26 @@ window.addEventListener("load", async () => {
 	    user_imports: {
 		document: {
 		    getElementById: (id) => document.getElementById(id),
+		    createTextNode: Document.prototype.createTextNode.bind(document),
+		    createComment: Document.prototype.createComment.bind(document),
+		    createElement: Document.prototype.createElement.bind(document),
+		    createTreeWalker: Document.prototype.createTreeWalker.bind(document)
 		},
 		element: {
 		    setInnerText: (elem, innerText) => elem.innerText = innerText,
 		    setInnerHTML: (elem, innerHtml) => elem.innerHTML = innerHtml,
 		    setAttribute: (elem, name, value) => elem.setAttribute(name, value),
 		    removeAttribute: (elem, name) => elem.removeAttribute(name),
+		    appendChild(parent, child) { return parent.appendChild(child); },
+		    remove(elem) { elem.remove(); },
+		    replaceWith(oldElem, newElem) { oldElem.replaceWith(newElem); }
+		},
+		treeWalker: {
+		    currentNode(walker) { return walker.currentNode; },
+		    setCurrentNode(walker, node) { walker.currentNode = node; },
+		    nextNode(walker) { return walker.nextNode(); },
+		    firstChild(walker) { return walker.firstChild(); },
+		    nextSibling(walker) { return walker.nextSibling(); }
 		},
 		event: {
 		    addEventListener: (target, type, listener) => target.addEventListener(type, listener),
